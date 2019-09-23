d3b7ef95acce9e0fb6c30a9045230dc50a5be42b
luifr <luifr@usp.br>
2019-09-23 00:25:49 -0300

    Changed upgrades return

--- src/controllers/upgrade.ts
+++ src/controllers/upgrade.ts
@@ -3,7 +3,7 @@ import * as Upgrade from '../model/upgrade';
 export async function get(req, res): Promise<void> {
 	let upgrade;
 	if (!req.query.upgrade)
-		upgrade = await Upgrade.getAll();
+		upgrade = { upgrades: await Upgrade.getAll() };
 	else
 		upgrade = await Upgrade.get(req.query.upgrade);
 	res.json(upgrade);
